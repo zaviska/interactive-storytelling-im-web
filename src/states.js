e3be67712f7b5bcc2d01c174d417968154d2487c
zaviska <jessica.zaviska@gmx.de>
2018-04-07 13:53:04 +0200

    cleanup states and objects

--- src/states.js
+++ src/states.js
@@ -8,7 +8,6 @@
 
 export {default as Boot} from './states/Boot';
 export {default as Preloader} from './states/Preloader';
-export {default as Game} from './states/Game';
 export {default as ToBeContinued} from './states/ToBeContinued';
 export {default as GameOver} from './states/GameOver';
 
